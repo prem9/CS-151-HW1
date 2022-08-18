83e7c16674daa96dbf769c92aada8c214b1613ec
Prem Agarwal <prem.m.agarwal@gmail.com>
2022-08-17 20:39:49 -0700

    taking break for class a bit

--- demo/src/main/java/com/example/Student.java
+++ demo/src/main/java/com/example/Student.java
@@ -6,9 +6,31 @@ package com.example;
  */
 public class Student 
 {
-    public static void main( String[] args )
+    
+    
+    private double price;
+    
+    public String lastName(String lastName)
     {
-        System.out.println( "Hello World!" );
+        return lastName;
     }
-    public 
+    public String firstName(string firstName)
+    {
+        return firstName;
+    }
+    public int age(int age)
+    {
+        return age;
+    }
+    public float gpa(float gpa)
+    {
+        return gpa;
+    }
+    public String toString()
+    {
+        String s = "";
+        s = "Plant[Name="+plantName+",Price="+price+"]";
+        return s;
+    }
 }
+
