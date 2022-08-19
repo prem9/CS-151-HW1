ef06438034a0ac61bfdfb1531d5443382ad8243a
Prem Agarwal <prem.m.agarwal@gmail.com>
2022-08-18 17:42:28 -0700

    finish tester and class

---
+++ demo/src/main/java/com/example/StudentTest.java
@@ -0,0 +1,29 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class StudentTest 
+{
+    
+    
+   public static void main(String[] args) {
+    // John Smith, 20 year old, 3.6 gpa, Computer Science major, School of Computer 
+    // Science department
+    // initializes student
+    Student student = new Student();
+    student.setAge(20);
+    student.setDepartment("School of Computer Science department");
+    student.setFirstName("John");
+    student.setGpa((float) 3.6);
+    student.setLastName("Smith");
+    student.setMajor("Computer Science Department");
+    // prints student
+    student.toString();
+    // prints schedule
+    Student.course course=student.new course();
+    course.printSchedule();
+   }
+}
+
